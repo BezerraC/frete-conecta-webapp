479ff6545aeaef3ff39ea8a6d745f6fad2d22a84
BezerraC <cbezerraneto@gmail.com>
2025-01-22 19:03:43 -0300

    feat: new testimonials

--- config/migrate.js
+++ config/migrate.js
@@ -7,9 +7,11 @@ const createUsersTable = async () => {
       fname VARCHAR(255) NOT NULL,
       lname VARCHAR(255) NOT NULL,
       phone VARCHAR(20) NOT NULL,
+      cpf VARCHAR(20) NOT NULL,
       email VARCHAR(255) UNIQUE NOT NULL,
       password VARCHAR(255) NOT NULL,
       type VARCHAR(50) DEFAULT 'cliente',
+      is_profile_complete BOOLEAN DEFAULT false,
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
